475656405f69cfcea678aca4e29b34dba8f17b26
Sree Anondo Kumar Roy <spkroy2006@gmail.com>
2026-07-31 10:26:19 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,11 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
+
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11462997
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
